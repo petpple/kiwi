898556e4abc1badc609347b4b648cee855183979
flown222 <jangu3384@gmail.com>
2023-01-02 21:10:22 +0900

    📑 :: AdminController 수정 -동현
    📑 :: AdminController 수정 -동현

--- src/main/java/com/petpple/kiwi/controller/Admin/AdminController.java
+++ src/main/java/com/petpple/kiwi/controller/Admin/AdminController.java
@@ -1,4 +1,4 @@
-package com.petpple.kiwi.controller;
+package com.petpple.kiwi.controller.Admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
