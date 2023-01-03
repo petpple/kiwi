5405dd4a687dbb52520d30f03d5a61edc5fb7683
2kkeullim <rosel0314@mail.hongik.ac.kr>
2023-01-03 14:37:00 +0900

    📑 :: VsitterController 생성 - 보영
    📑 :: VsitterController 생성 - 보영

---
+++ src/main/java/com/petpple/kiwi/controller/Vsitter/VsitterController.java
@@ -0,0 +1,21 @@
+package com.petpple.kiwi.controller.Vsitter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VsitterController {
+
+	@RequestMapping("/sitter")
+	public String vsitterMain()
+	{
+		return "vsitter/vsitterMain";
+	}
+	
+	@RequestMapping("/side")
+	public String vsitterSide()
+	{
+		return "vsitter/vsitterSide";
+	}
+	
+}
