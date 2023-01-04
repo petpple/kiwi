3915ae4173cbcdf2a9ca9d9709ab8882bb3dcb5f
younggwan <johnbuuk1@gmail.com>
2023-01-04 17:36:22 +0900

    ⚡️ :: 펫시터 마이페이지 매핑 - 영관
    ⚡️ :: 펫시터 마이페이지 매핑 - 영관

--- src/main/java/com/petpple/kiwi/controller/Fsitter/FsitterController.java
+++ src/main/java/com/petpple/kiwi/controller/Fsitter/FsitterController.java
@@ -1,8 +1,44 @@
 package com.petpple.kiwi.controller.Fsitter;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FsitterController {
-
+	
+	@RequestMapping("/fsitterMyProfile")
+	public String fsitterMyProfile()
+	{
+		return "fsitter/fsitterMyProfile";
+	}
+	
+	@RequestMapping("/fsitterMyBank")
+	public String fsitterMyBank()
+	{
+		return "fsitter/fsitterMyBank";
+	}
+	
+	@RequestMapping("/fsitterSide")
+	public String fsitterSide()
+	{
+		return "fsitter/fsitterSide";
+	}
+	
+	@RequestMapping("/fsitterHeader")
+	public String fsitterHeader()
+	{
+		return "fsitter/fsitterHeader";
+	}
+	
+	@RequestMapping("/fsitterMyBlock")
+	public String fsitterMyBlock()
+	{
+		return "fsitter/fsitterMyBlock";
+	}
+	
+	@RequestMapping("/fsitterMyDrawl")
+	public String fsitterMyDrawl()
+	{
+		return "fsitter/fsitterMyDrawl";
+	}
 }
