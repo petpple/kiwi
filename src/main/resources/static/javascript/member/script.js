c68e13bd56a9db9b4f00e316e081546d9b9c0450
SOYEON <tivm8012@naver.com>
2023-01-05 11:08:41 +0900

    📑 ::  js2 생성(모달팝업)  - 소연
    📑 ::  js2 생성(모달팝업)  - 소연

---
+++ src/main/resources/static/javascript/member/script.js
@@ -0,0 +1,13 @@
+// Javascript Document
+$(document).ready(function(){  
+
+/* 팝업창 */
+	$('.btn').click(function(){
+		$('.mlp').show();	
+	});
+	$('.close').click(function(){
+		$('.mlp').hide();	
+	});
+	
+	
+});
