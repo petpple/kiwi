94efbce0ebc568ff0e9cf7fea34df57b7cb7c6a8
SOYEON <tivm8012@naver.com>
2023-01-05 11:03:50 +0900

    ⚡️ ::  memberController 매핑 - 소연
    ⚡️ ::  memberController 매핑 - 소연

--- src/main/java/com/petpple/kiwi/controller/Member/MemberController.java
+++ src/main/java/com/petpple/kiwi/controller/Member/MemberController.java
@@ -37,7 +37,35 @@ public class MemberController
 	{
 		return "member/sitterAllowSubmit";
 	}
-
 	
+	@RequestMapping("/member/sitterFos")
+	public String sitterFos()
+	{
+		return "member/sitterFos";
+	}
+	
+	@RequestMapping("/member/sitterFosForm")
+	public String sitterFosForm()
+	{
+		return "member/sitterFosForm";
+	}
+	
+	@RequestMapping("/member/fSitterIn")
+	public String fSitterIn()
+	{
+		return "member/fSitterIn";
+	}
+	
+	@RequestMapping("/member/memberReview")
+	public String memberReview()
+	{
+		return "member/memberReview";
+	}
+
+	@RequestMapping("/member/memberReviewT")
+	public String memberReviewT()
+	{
+		return "member/memberReviewT";
+	}
 	
 }
